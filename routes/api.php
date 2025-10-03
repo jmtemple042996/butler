59cffdaa712e6b5c4313bc32e3604c05da4a681c
James Matthew Temple <jmtemple0429@gmail.com>
2025-10-03 12:04:43 -0400

    push

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Setting;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('adopt', function(Request $request) {
+    $attributes = $request->validate([
+        'host' => ['url','required'],
+        'key' => ['required']
+    ]);
+
+    Setting::upsert([
+        ['name' => 'master:host','value' => $attributes['host']],
+        ['name' => 'master:key','value' => $attributes['key']],
+    ], ['key'],['name']);
+});
